856b97443c61152349d79c93e8d147005a7f7148
ruks <ruky68@live.com>
2021-04-27 14:28:52 +0530

    Fixing internal issue when useing graphQL fragments

--- components/apimgt/org.wso2.carbon.apimgt.gateway/src/main/java/org/wso2/carbon/apimgt/gateway/handlers/graphQL/GraphQLAPIHandler.java
+++ components/apimgt/org.wso2.carbon.apimgt.gateway/src/main/java/org/wso2/carbon/apimgt/gateway/handlers/graphQL/GraphQLAPIHandler.java
@@ -193,6 +193,9 @@ public class GraphQLAPIHandler extends AbstractHandler {
     public void getNestedLevelOperations(List<Selection> selectionList, ArrayList<String> supportedFields,
                                 ArrayList<String> operationArray) {
         for (Selection selection : selectionList) {
+            if (!(selection instanceof Field)) {
+                continue;
+            }
             Field levelField = (Field) selection;
             if (!operationArray.contains(levelField.getName()) &&
                     supportedFields.contains(levelField.getName())) {
