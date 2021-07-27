644f7c3328c6cfdc2ec20918eed3f8daaf510974
Tharika Madurapperuma <tharikaGitHub@users.noreply.github.com>
2021-07-27 17:33:37 +0530

    Revert "Added U2 missing PR's to x full branch"

--- components/apimgt/org.wso2.carbon.apimgt.impl/src/main/java/org/wso2/carbon/apimgt/impl/dto/JWTConfigurationDto.java
+++ components/apimgt/org.wso2.carbon.apimgt.impl/src/main/java/org/wso2/carbon/apimgt/impl/dto/JWTConfigurationDto.java
@@ -30,7 +30,6 @@ public class JWTConfigurationDto {
     private String signatureAlgorithm = "SHA256withRSA";
     private String jwtGeneratorImplClass = "org.wso2.carbon.apimgt.keymgt.token.JWTGenerator";
     private String claimRetrieverImplClass;
-    private boolean enableUserClaims;
     private String gatewayJWTGeneratorImpl;
     private Map<String,TokenIssuerDto> tokenIssuerDtoMap = new HashMap();
     private Set<String> jwtExcludedClaims = new HashSet<>();
@@ -136,7 +135,7 @@ public class JWTConfigurationDto {
 
         this.jwtExcludedClaims = jwtClaims;
     }
-    
+
     public void setEnableUserClaimRetrievalFromUserStore(boolean enableUserClaimRetrievalFromUserStore) {
 
         this.enableUserClaimRetrievalFromUserStore = enableUserClaimRetrievalFromUserStore;
@@ -145,15 +144,5 @@ public class JWTConfigurationDto {
     public boolean isEnableUserClaimRetrievalFromUserStore() {
 
         return enableUserClaimRetrievalFromUserStore;
-    }
-
-    public boolean isEnableUserClaims() {
-
-        return enableUserClaims;
-    }
-
-    public void setEnableUserClaims(boolean enableUserClaims) {
-
-        this.enableUserClaims = enableUserClaims;
     }
 }
