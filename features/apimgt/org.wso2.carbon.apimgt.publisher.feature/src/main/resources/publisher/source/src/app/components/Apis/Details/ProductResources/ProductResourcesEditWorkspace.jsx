118653c416c0c3b953b09577d4df5bd3efecc3ce
HiranyaKavishani <hiranyakavi@gmail.com>
2022-01-05 13:12:12 +0530

    [U2] Fixing issue when product resource selection (#4669)
    * Fixing issue when product resource selection

--- features/apimgt/org.wso2.carbon.apimgt.publisher.feature/src/main/resources/publisher/source/src/app/components/Apis/Details/ProductResources/ProductResourcesEditWorkspace.jsx
+++ features/apimgt/org.wso2.carbon.apimgt.publisher.feature/src/main/resources/publisher/source/src/app/components/Apis/Details/ProductResources/ProductResourcesEditWorkspace.jsx
@@ -178,6 +178,15 @@ const useStyles = makeStyles((theme) => ({
     },
 }));
 
+const resourceMethod = {
+    POST: 'post',
+    PUT: 'put',
+    GET: 'get',
+    DELETE: 'delete',
+    PATCH: 'patch',
+    OPTIONS: 'options',
+    HEAD: 'head',
+};
 
 /**
  *
@@ -232,39 +241,44 @@ function ProductResourcesEdit(props) {
             Object.keys(methodObj).map((innerKey) => {
                 // We are setting the check property at this level because we need to
                 // add resources for each verb ( post, get, put etc.. )
-                methodObj[innerKey].checked = false;
+                if (innerKey === resourceMethod.DELETE || innerKey === resourceMethod.POST
+                || innerKey === resourceMethod.GET || innerKey === resourceMethod.PUT
+                || innerKey === resourceMethod.PATCH || innerKey === resourceMethod.OPTIONS
+                || innerKey === resourceMethod.HEAD) {
+                    methodObj[innerKey].checked = false;
 
-                // We need to check the latestApiResources for the same
-                // API/Resource Name / Verb and  indicate it differently
-                // Loop latestApiResources object
-                const target = key;
-                const verb = innerKey;
-                let resourceFound = false;
-                if (latestApiResources) {
-                    Object.keys(latestApiResources).map((resourcekey) => {
-                        const apiResource = latestApiResources[resourcekey];
+                    // We need to check the latestApiResources for the same
+                    // API/Resource Name / Verb and  indicate it differently
+                    // Loop latestApiResources object
+                    const target = key;
+                    const verb = innerKey;
+                    let resourceFound = false;
+                    if (latestApiResources) {
+                        Object.keys(latestApiResources).map((resourcekey) => {
+                            const apiResource = latestApiResources[resourcekey];
 
-                        // Check if the the api slected from UI is same as the operation api id checking
-                        if (apiResource && apiId === apiResource.apiId) {
-                            // API is the same
-                            Object.keys(apiResource.operations).map((operationKey) => {
-                                const operation = apiResource.operations[operationKey];
-                                if (
-                                    operation
-                                    && operation.target === target
-                                    && operation.verb.toLowerCase() === verb.toLowerCase()
-                                ) {
-                                    // Operation is already there
-                                    resourceFound = true;
-                                }
-                            });
-                        }
-                    });
-                }
-                if (resourceFound) {
-                    methodObj[innerKey].allreadyAdded = true;
-                } else {
-                    methodObj[innerKey].allreadyAdded = false;
+                            // Check if the the api slected from UI is same as the operation api id checking
+                            if (apiResource && apiId === apiResource.apiId) {
+                                // API is the same
+                                Object.keys(apiResource.operations).map((operationKey) => {
+                                    const operation = apiResource.operations[operationKey];
+                                    if (
+                                        operation
+                                        && operation.target === target
+                                        && operation.verb.toLowerCase() === verb.toLowerCase()
+                                    ) {
+                                        // Operation is already there
+                                        resourceFound = true;
+                                    }
+                                });
+                            }
+                        });
+                    }
+                    if (resourceFound) {
+                        methodObj[innerKey].allreadyAdded = true;
+                    } else {
+                        methodObj[innerKey].allreadyAdded = false;
+                    }
                 }
             });
         });
