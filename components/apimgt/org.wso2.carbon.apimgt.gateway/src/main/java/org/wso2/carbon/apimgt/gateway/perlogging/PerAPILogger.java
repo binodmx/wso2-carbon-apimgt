8ed3c11ee7fba5b89c5d0ea178aa5bd99e2825b2
rashm1n <rashmin.ravindu@gmail.com>
2022-05-25 09:40:31 +0530

    Formatting and License Header Changes

--- components/apimgt/org.wso2.carbon.apimgt.gateway/src/main/java/org/wso2/carbon/apimgt/gateway/perlogging/PerAPILogger.java
+++ components/apimgt/org.wso2.carbon.apimgt.gateway/src/main/java/org/wso2/carbon/apimgt/gateway/perlogging/PerAPILogger.java
@@ -1,20 +1,18 @@
 /*
-        * Copyright (c) 2020 WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
-        *
-        * WSO2 Inc. licenses this file to you under the Apache License,
-        * Version 2.0 (the "License"); you may not use this file except
-        * in compliance with the License.
-        * You may obtain a copy of the License at
-        *
-        *   http://www.apache.org/licenses/LICENSE-2.0
-        *
-        * Unless required by applicable law or agreed to in writing,
-        * software distributed under the License is distributed on an
-        * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
-        * KIND, either express or implied.  See the License for the
-        * specific language governing permissions and limitations
-        * under the License.
-        */
+ * Copyright (c) 2022, WSO2 Inc.(http://www.wso2.org) All Rights Reserved.
+ * WSO2 Inc. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * */
 
 package org.wso2.carbon.apimgt.gateway.perlogging;
 
@@ -34,7 +32,6 @@ public class PerAPILogger implements PerAPILogService {
     private static PerAPILogService perAPILogService = new PerAPILogger();
     private static String streamId = "org.wso2.apimgt.perapi.log.stream:1.0.0";
 
-
     private PerAPILogger() {
     }
 
@@ -47,7 +44,8 @@ public class PerAPILogger implements PerAPILogService {
 
     @Override public void publishLogAPIData(String context, String value) {
         Object[] objects = new Object[] { context, value };
-        Event perapilogmessage = new Event(streamId, System.currentTimeMillis(), null, null, objects);
+        Event perapilogmessage =
+                new Event(streamId, System.currentTimeMillis(), null, null, objects);
         ServiceReferenceHolder.getInstance().getThrottleDataPublisher().getDataPublisher().tryPublish(perapilogmessage);
     }
 
