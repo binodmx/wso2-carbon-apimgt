cbef8e1f6ad94872cf5294cab4d1b5c8b4069710
Chanaka Jayasena <chanakajayasena@chanaka.local>
2022-07-19 08:45:55 +0530

    Adding TOKEN_TYPE to other database types

--- components/apimgt/org.wso2.carbon.apimgt.impl/src/main/java/org/wso2/carbon/apimgt/impl/dao/constants/SQLConstantPostgreSQL.java
+++ components/apimgt/org.wso2.carbon.apimgt.impl/src/main/java/org/wso2/carbon/apimgt/impl/dao/constants/SQLConstantPostgreSQL.java
@@ -125,7 +125,8 @@ public class SQLConstantPostgreSQL extends SQLConstants{
                     "   USER_ID, " +
                     "   GROUP_ID, " +
                     "   UUID, " +
-                    "   APP.CREATED_BY AS CREATED_BY " +
+                    "   APP.CREATED_BY AS CREATED_BY, " +
+                    "   APP.TOKEN_TYPE AS TOKEN_TYPE " +
                     " FROM" +
                     "   AM_APPLICATION APP, " +
                     "   AM_SUBSCRIBER SUB  " +
